c8f11ba6f99ecee57f67d1f144b020ac932bda95
Jared A. Scheel <jared@jaredscheel.com>
2012-06-25 08:18:55 -0500

    moved header.less to variables.less and added some basic coffeescript documentation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).on('pop-initialized', function(){
   // Instead of listening for the document.ready event, your theme
   // should listen for document.pop-initialized.
+  // 
+  // We are huge fans of coffeescript at Populr. We suggest that 
+  // you write your js in coffeescript, but please be sure to 
+  // pre-compile it, as Populr will not compile your
+  // coffeescript to js code automatically.
 });
